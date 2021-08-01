3966b7be021fadd9d7591c6be4ba2d48825e9968
Alison Ferreira <88048825+DeviAlison@users.noreply.github.com>
2021-08-01 10:53:10 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,104 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12123',
+                nome: 'Abmael da Silva',
+                partido: 'PDT',
+                fotos:[
+                    {url:'v1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'13451',
+                nome: 'Ademir',
+                partido: 'PT',
+                fotos:[
+                    {url:'v2.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '20111',
+                nome: 'Ademir da Radio',
+                partido: 'PSC',
+                fotos:[
+                    {url:'v3.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '19100',
+                nome: 'Ademir do Açougue',
+                partido: 'PODE',
+                fotos:[
+                    {url:'v4.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '33888',
+                nome: 'Adriana de Almeida',
+                partido: 'PMN',
+                fotos:[
+                    {url:'v5.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos:[
+            {
+                numero:'45',
+                nome: 'Dr Heber',
+                partido: 'PSDB',
+                vice: 'Rodrigo Borges',
+                fotos:[
+                    {url:'cp1.jpg', legenda: 'Prefeito'},
+                    {url:'cv1.jpg', legenda:  'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'13',
+                nome: 'Dr Éverton',
+                partido: 'PT',
+                vice: 'Dilma Sabtaba',
+                fotos:[
+                    {url:'cp2.jpg', legenda: 'Prefeito'},
+                    {url:'cv2.jpg', legenda:  'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'12',
+                nome: 'Salma Regina Galate',
+                partido: 'PDT',
+                vice: 'Renata Rocha',
+                fotos:[
+                    {url:'cp3.jpg', legenda: 'Prefeito'},
+                    {url:'cv3.jpg', legenda:  'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'77',
+                nome: 'Jorginho',
+                partido: 'SOLIDARIEDADE',
+                vice: 'Professora Elisângela',
+                fotos:[
+                    {url:'cp4.jpg', legenda: 'Prefeito'},
+                    {url:'cv4.jpg', legenda:  'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'15',
+                nome: 'Toninho Zeitun',
+                partido: 'MDB',
+                vice: 'Barretinho',
+                fotos:[
+                    {url:'cp5.jpg', legenda: 'Prefeito'},
+                    {url:'cv5.jpg', legenda:  'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
